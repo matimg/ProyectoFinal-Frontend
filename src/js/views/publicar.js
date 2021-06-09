37aeae9fb178438485e3629275c61f493135d7ec
leandro3742 <leandro.marrero@outlook.com>
2021-06-09 13:37:45 +0000

    publicacion terminada

--- src/js/views/publicar.js
+++ src/js/views/publicar.js
@@ -14,48 +14,79 @@ import { Select } from "react-bootstrap";
 
 import "../../styles/publicar.scss";
 
+import { Image, Video } from "cloudinary-react";
+
 export const Publicar = () => {
 	const { store, actions } = useContext(Context);
 	const [validated, setValidated] = useState(false);
 
+	const publicacion = async () => {
+		let result = await actions.publicar(titulo, descripcion, url, categoria);
+		console.log(result);
+	};
+	const publicar = (titulo, descripcion, file, categoria) => {
+		const data = new FormData();
+		data.append("file", file);
+		data.append("upload_preset", "ml_default");
+		data.append("cloud_name", "ecomproyecto");
+		fetch("https://api.cloudinary.com/v1_1/ecomproyecto/image/upload", {
+			method: "post",
+			body: data
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				console.log(data);
+				console.log(data.secure_url);
+				let url = data.secure_url;
+
+				actions.publicar(titulo, descripcion, url, categoria);
+			})
+			.catch(err => console.log(err));
+	};
 	const handleSubmit = event => {
 		const form = event.currentTarget;
+		event.preventDefault();
 		if (form.checkValidity() === false) {
 			event.preventDefault();
 			event.stopPropagation();
 		}
-
+		let titulo = form.titulo.value;
+		let descripcion = form.descripcion.value;
+		let file = form.url.files[0];
+		let categoria = form.categoria.value;
 		setValidated(true);
+		publicar(titulo, descripcion, file, categoria);
 	};
 
 	return (
 		<div className="container-sm justify-content-center align-items-center cuerpoPublicar">
 			<div className="contenedorPublicar justify-content-center align-items-center rounded">
 				<Form noValidate validated={validated} onSubmit={handleSubmit}>
-					<Form.Group className="" controlId="exampleForm.ControlInput1">
+					<Form.Group className="" controlId="titulo">
 						<Form.Label className="fuentePublicar">Título de la publicación</Form.Label>
 						<Form.Control type="text" placeholder="Ejemplo: Mi titulo" required />
 						<Form.Control.Feedback type="invalid">Ingrese un titulo</Form.Control.Feedback>
 					</Form.Group>
-					<Form.Group className="" controlId="exampleForm.ControlTextarea1">
+					<Form.Group className="" controlId="descripcion">
 						<Form.Label className="fuentePublicar">Describe tu publicación</Form.Label>
 						<Form.Control as="textarea" rows={3} placeholder="Descripcion..." required />
 						<Form.Control.Feedback type="invalid">Ingrese una descripción</Form.Control.Feedback>
 					</Form.Group>
 					<div className="row">
 						<div className="col">
-							<Form.Group controlId="formFileMultiple" className="mb-3">
+							<Form.Group controlId="url" className="mb-3">
 								<Form.Label>Sube tu archivo aquí</Form.Label>
 								<Form.Control type="file" className="border-dark" required />
+								{/* <input type="file" onChange= {(e)=> setImage(e.target.files[0])}></input> */}
 								<Form.Control.Feedback type="invalid">Sube un archivo</Form.Control.Feedback>
 							</Form.Group>
 						</div>
 						<div className="col">
-							<Form.Control as="select">
+							<Form.Control as="select" id="categoria">
 								<option>Open this select menu</option>
-								<option value="1">Video</option>
-								<option value="2">Imagen</option>
-								<option value="3">Sonido</option>
+								<option value="Video">Video</option>
+								<option value="Image">Imagen</option>
+								<option value="Sonido">Sonido</option>
 							</Form.Control>
 						</div>
 					</div>
