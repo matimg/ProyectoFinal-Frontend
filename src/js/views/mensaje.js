e53f100e27a5586358d703cef66a80fd35f7846b
Matías Gandini <matias.gandinimg@gmail.com>
2021-06-16 19:50:46 +0000

    Envio de mensajes mejorado

--- src/js/views/mensaje.js
+++ src/js/views/mensaje.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { Link, useParams } from "react-router-dom";
 import "../../styles/mensaje.scss";
 import { Context } from "../store/appContext";
@@ -28,34 +28,52 @@ export const Mensajes = () => {
 		getMensajes();
 	}, []);
 
+	const messagesEndRef = useRef(null);
+	const scrollToBottom = () => {
+		messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+	};
+	useEffect(
+		() => {
+			scrollToBottom();
+		},
+		[mensajes]
+	);
+
 	return (
 		<div className="container d-flex justify-content-center align-items-center" id="fondoMensajes">
 			<div className="container rounded p-0 " id="contenedorMensajes">
 				<div
+					style={{ height: "15%" }}
 					className="rounded-top d-flex justify-content-center align-items-center mb-3"
 					id="tituloContenedor">
 					<h5 className="text-white">Mensajes</h5>
 				</div>
-				{mensajes.map((elem, iterador) => {
-					if (elem.usuarioEmisor.id === usuario.id) {
-						return (
-							<div key={iterador} className="d-flex justify-content-start">
-								<div className="text-white px-2 ml-3 py-2 my-1 text-center mensaje">{elem.mensaje}</div>
-							</div>
-						);
-					} else {
-						return (
-							<div key={iterador} className="d-flex justify-content-end">
-								<div className="text-white px-2 mr-3 py-2 my-1 text-center mensaje2">
-									{elem.mensaje}
+				<div style={{ height: "70%", overflowY: "scroll", scrolltop: "300px" }}>
+					{mensajes.map((elem, iterador) => {
+						if (elem.usuarioEmisor.id === usuario.id) {
+							return (
+								<div key={iterador} className="d-flex justify-content-start">
+									<div className="text-white px-2 ml-3 py-2 my-1 text-center mensaje">
+										{elem.mensaje}
+									</div>
 								</div>
-							</div>
-						);
-					}
-				})}
+							);
+						} else {
+							return (
+								<div key={iterador} className="d-flex justify-content-end">
+									<div className="text-white px-2 mr-3 py-2 my-1 text-center mensaje2">
+										{elem.mensaje}
+									</div>
+								</div>
+							);
+						}
+					})}
+					<div ref={messagesEndRef} />
+				</div>
 				<form
+					style={{ height: "15%" }}
 					onSubmit={() => enviarMensaje(event)}
-					className="container-fluid d-flex justify-content-center align-items-center rounded-bottom mt-5"
+					className="container-fluid d-flex justify-content-center align-items-center rounded-bottom mt-1"
 					id="formulario">
 					<textarea
 						className="rounded mx-0 mb-1 mt-1 border-0"
