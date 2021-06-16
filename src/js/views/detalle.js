288f16527500cfbc53f8ce7399b983c44aed747d
Matías Gandini <matias.gandinimg@gmail.com>
2021-06-16 18:56:04 +0000

    Boton contacto redirecciona a mensajes

--- src/js/views/detalle.js
+++ src/js/views/detalle.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import "../../styles/detalle.scss";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { Alert } from "bootstrap";
 
 export const Detalle = () => {
 	const { store, actions } = useContext(Context);
@@ -20,78 +21,47 @@ export const Detalle = () => {
 	useEffect(() => {
 		traerDetalle();
 	}, []);
-	return (
-		<div className="container d-flex justify-content-center align-items-center modificarHeight">
-			<div
-				className="container rounded mt-5"
-				id="cuerpoDetalle"
-				style={{ backgroundImage: `url(${publicacion.url})` }}>
-				<div className="container text-white" id="textoDetalle">
-					<h3>{publicacion.titulo}</h3>
-					<p>{publicacion.descripcion}</p>
-					<div className="container ml-1">
-						<div className="rounded-pill bg-dark" id="nombreDetalle">
-							<p className="mx-2" id="soloTexto">
-								{usuario.nombre} {usuario.apellido}
-							</p>
-						</div>
-						<div id="iconoDetalle">
-							<i className="far fa-comment-dots" id="iconoDetalle2" />
+
+	if (publicacion.formato == "image") {
+		return (
+			<div className="container d-flex justify-content-center align-items-center modificarHeight">
+				<div
+					className="container rounded mt-5"
+					id="cuerpoDetalle"
+					style={{ backgroundImage: `url(${publicacion.url})` }}>
+					<div className="container text-white" id="textoDetalle">
+						<h3>{publicacion.titulo}</h3>
+						<p>{publicacion.descripcion}</p>
+						<div className="container ml-1">
+							<div className="rounded-pill bg-dark" id="nombreDetalle">
+								<p className="mx-2" id="soloTexto">
+									{usuario.nombre} {usuario.apellido}
+								</p>
+							</div>
+							<div id="iconoDetalle">
+								<Link to={"/mensajes/" + usuario.id}>
+									<i className="far fa-comment-dots" id="iconoDetalle2" type="button" />
+								</Link>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-
-			{/*<div className="card mb-3 bg-light" id="cartaCuerpo" style={{ backgroundImage: `url(${publicacion.url})` }}>
-				<div className="row">
-					<div className="col-md-9 d-flex justify-content-center align-items-center">
-						<img src={publicacion.url} alt="..." id="imagenDetalle" />
-					</div>
-					<div className="col-md-3">
-						<div className="card-body px-1 px-md-0">
-							<h5 className="card-title">{publicacion.titulo}</h5>
-							<p className="card-text">{publicacion.descripcion}</p>
-							<p className="card-text">
-								<small className="text-muted">
-									{usuario.nombre} {usuario.apellido}
-								</small>
-							</p>
-							<p className="card-text" id="contacto">
-								Contacta a este usuario{" "}
-							</p>
-						</div>
+		);
+	} else {
+		return (
+			<div className="container d-flex flex-column justify-content-center align-items-center modificarHeight">
+				<video className="rounded mt-2" style={{ width: "75%" }} src={publicacion.url} alt="" controls />
+				<div style={{ width: "75%" }} className="d-flex flex-column justify-content-start bg-secondary">
+					<h3 className="text-start">{publicacion.titulo}</h3>
+					<div className="d-flex">
+						<p>{publicacion.descripcion}</p>
+						<p className="ml-3">
+							{usuario.nombre} {usuario.apellido}
+						</p>
 					</div>
 				</div>
-			</div>*/}
-			{/* <div className="card bg-transparent text-white border-0 contenedorCarta">
-				<img src={publicacion.url} className="card-img imagen" alt="..." />
-				<div className="card-img-overlay textoDetalle container">
-					<div className="row rounded-top" id="fondoOpacidad">
-						<div className="col">
-							<h5 className="card-title">{publicacion.titulo}</h5>
-						</div>
-					</div>
-					<div className="row" id="fondoOpacidad">
-						<div className="col">
-							<p className="card-text">{publicacion.descripcion}</p>
-						</div>
-					</div>
-					<div className="row" id="fondoOpacidad">
-						<div className="col">
-							<p className="card-text">
-								{usuario.nombre} {usuario.apellido}
-							</p>
-						</div>
-					</div>
-					<div className="row rounded-bottom" id="fondoOpacidad">
-						<div className="col">
-							<p className="card-text" id="contacto">
-								Contacta a este usuario{" "}
-							</p>
-						</div>
-					</div>
-				</div>
-			</div> */}
-		</div>
-	);
+			</div>
+		);
+	}
 };
